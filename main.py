40b019966b2fb51ff0de907115e496aadbea2673
Jin Tao <jin.tao@silo.ai>
2023-08-17 12:38:09 +0300

    delete password for gmail

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import lxml
 import smtplib
 
 MY_EMAIL = "jintao.helsinki@gmail.com"
-PASSWORD = "ypoocvmigxovpvzo"
+PASSWORD = "Your app password"
 URL = "https://www.amazon.de/-/en/ARZOPA-Portable-Monitor-External-Black-1/dp/B092KKLH93?ref_=Oct_d_obs_d_429868031_0&pd_rd_w=s4Trn&content-id=amzn1.sym.e29f2335-2e49-4147-8cf3-d7e9bd9a576a&pf_rd_p=e29f2335-2e49-4147-8cf3-d7e9bd9a576a&pf_rd_r=TKAF4A3YWFAQBW99WN77&pd_rd_wg=dvMUi&pd_rd_r=8cd8212c-eaa3-448e-b4f8-e51cee1fb7ad&pd_rd_i=B092KKLH93"
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
